a252c5aa8c2454f1cc98aff2f202a764ea6eaec8
kusogakiller <negiradomoti@proton.me>
2026-08-15 22:59:47 +0800

    chore: remove investigation temp file agg_probe.h

--- agg_probe.h
+++
@@ -1,4 +0,0 @@
-typedef struct { int x; int y; } Point2;
-typedef struct { Point2 origin; int count; int vals[4]; char name[16]; } Aggregate;
-typedef struct { int id; int len; double data[]; } FlexBuf;
-void use(Aggregate*a, FlexBuf*f){}
